ae05d530e22a0bac0785db5ba6282c2c286c9414
Thushar Nikam <43693630+tusharnikam07@users.noreply.github.com>
2024-12-28 18:50:53 +0530

    method to handle an unknown amount of numbers
    Modified add method to handle an unknown amount of numbers

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -4,18 +4,25 @@ public class StringCalculator {
 		if (numbers == null || numbers.isEmpty()) {
 			return 0;
 		}
+		int sum = 0;
 		String[] tokens = numbers.split(",");
-
-		int firstNum = Integer.parseInt(tokens[0].trim());
-		int secondNum = Integer.parseInt(tokens[1].trim());
-
-		return firstNum + secondNum;
+		for (String token : tokens) {
+			if (!token.isEmpty()) {
+				int num = Integer.parseInt(token);
+				sum += num;
+			}
+		}
+		return sum;
 	}
 
 	public static void main(String[] args) {
 		StringCalculator calculator = new StringCalculator();
 		System.out.println(calculator.Add(""));
+		System.out.println(calculator.Add("1"));
 		System.out.println(calculator.Add("1,2"));
+		System.out.println(calculator.Add("1,2,3,4,5,6"));
+		System.out.println(calculator.Add("1,2,3,4,5,6,7,8,9,10,11,12,13"));
+
 	}
 
 }
