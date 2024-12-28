47543ee333cdef6dcbc7df73e12d372bab37be5b
Thushar Nikam <43693630+tusharnikam07@users.noreply.github.com>
2024-12-28 19:08:27 +0530

    Supporting custom delimeters
    Wrote logic to change the delimiter if the beginning of the string will contain a separate line that looks like this: “//[delimiter]\n[numbers…]” for example “//;\n1;2”

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -6,6 +6,13 @@ public class StringCalculator {
 		}
 		int sum = 0;
 		String delimeter = ",|\\n";
+
+		if (numbers.startsWith("//")) {
+			int delimiterEnd = numbers.indexOf("\n");
+			delimeter = numbers.substring(2, delimiterEnd);
+			numbers = numbers.substring(delimiterEnd + 1);
+		}
+
 		if (numbers.contains(",\n") || numbers.contains("\n,")) {
 			throw new IllegalArgumentException(
 					"Invalid input format: mixed commas and newlines without proper separation");
@@ -27,7 +34,8 @@ public class StringCalculator {
 		System.out.println(calculator.Add("1"));
 		System.out.println(calculator.Add("1,2"));
 		System.out.println(calculator.Add("1\n2,3"));
-		System.out.println(calculator.Add("1,\n2"));
+//		System.out.println(calculator.Add("1,\n2"));
+		System.out.println(calculator.Add("//;\n1;2"));
 
 	}
 
